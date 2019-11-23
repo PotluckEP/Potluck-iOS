69259231ae8d4ca502492fbee3dc83ba53518319
Daniel Morales <danielmorales0000@yahoo.com>
2019-11-22 17:34:36 -0800

    Revert "Sign in fix branch"

--- Potluck/Sign In/SignInViewController.swift
+++ Potluck/Sign In/SignInViewController.swift
@@ -11,24 +11,18 @@ import GoogleSignIn
 
 class SignInViewController: UIViewController{
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Google Sign In stuff
         GIDSignIn.sharedInstance()?.presentingViewController = self
         // Automatically sign in the user.
         GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        let gSignIn = GIDSignInButton(frame: CGRect(x: 0, y: 0, width: 230, height: 48))
+        gSignIn.center = view.center
+        view.addSubview(gSignIn)
     }
     
-    @IBAction func testButtonClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toMenu", sender: self)
-    }
-    @IBAction func googleSignButtonClicked(_ sender: Any) {
-        print("sup")
-        performSegue(withIdentifier: "toMenu", sender: self)
-    
-    }
-    //    @IBAction func didTapSignOut(_ sender: AnyObject) {
+//    @IBAction func didTapSignOut(_ sender: AnyObject) {
 //        GIDSignIn.sharedInstance().signOut()
 //        let firebaseAuth = Auth.auth()
 //        do {
