b8a50aa8543244e65f8176aeecf16048576b4810
Andrea Amezcua Moreno <andrea@Andreas-MBP-2.attlocal.net>
2019-12-10 13:32:01 -0800

    added video

--- Potluck/Sign In/SignInViewController.swift
+++ Potluck/Sign In/SignInViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 import GoogleSignIn
+import AVFoundation
+import AVKit
 
 class SignInViewController: UIViewController{
+    
+    @IBOutlet weak var videoView: UIView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //background video
+        setupView()
+        
         // Google Sign In stuff
         GIDSignIn.sharedInstance()?.presentingViewController = self
         // Automatically sign in the user.
@@ -25,6 +34,34 @@ class SignInViewController: UIViewController{
     @IBAction func googleSignButtonClicked(_ sender: Any) {
         performSegue(withIdentifier: "toMenu", sender: self)
     }
+    
+    
+    private func setupView(){
+        
+        guard let path = Bundle.main.path(forResource: "Sign in Video", ofType:"mov")
+        else{
+            return
+        }
+       
+        let player = AVPlayer(url: NSURL(fileURLWithPath: path) as URL)
+        
+        let newLayer = AVPlayerLayer(player: player)
+        newLayer.frame = self.videoView.frame
+        self.videoView.layer.addSublayer(newLayer)
+        newLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        player.play()
+        player.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(SignInViewController.videoDidPlayToEnd(_:)),name: NSNotification.Name(rawValue: "AVPlayerItemDidPlayToEndTimeNotification"), object: player.currentItem)
+    }
+    
+    @objc func videoDidPlayToEnd(_ notification: Notification){
+        let player: AVPlayerItem = notification.object as! AVPlayerItem
+        //player.seek(to: KCMTimeZero)
+        player.seek(to: CMTime.zero)
+    }
+    
 
     /*
     // MARK: - Navigation
